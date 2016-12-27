30863ed2d11fb97d5e67c36abfe500a768d9ace8
alvi17 <alvi1005017@gmail.com>
2016-12-27 07:47:50 +0600

    release 1.1

--- app/src/main/java/alvi17/brightflashlight/MainActivity.java
+++ app/src/main/java/alvi17/brightflashlight/MainActivity.java
@@ -1,5 +1,6 @@
 package alvi17.brightflashlight;
 
+import android.Manifest;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -19,6 +20,8 @@ import android.os.Build;
 
 import android.os.IBinder;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -27,6 +30,7 @@ import android.util.Size;
 import android.view.Gravity;
 import android.view.Surface;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.FrameLayout;
@@ -43,7 +47,7 @@ import com.google.android.gms.ads.AdView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements  ActivityCompat.OnRequestPermissionsResultCallback{
 
     private CameraCaptureSession mSession;
     private CaptureRequest.Builder mBuilder;
@@ -52,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
     boolean isFlashLightOn = false;
     private Intent serviceIntent;
     FlashLightService flashLightService;
-    CheckBox checkBox;
+    public  CheckBox checkBox;
     TextView trun_on_off;
 
     private final ServiceConnection flashServiceConnection = new ServiceConnection() {
@@ -72,6 +76,20 @@ public class MainActivity extends AppCompatActivity {
     FrameLayout fm;
     AdView adView;
 
+    private static final String[] PERMISSIONS = {
+            Manifest.permission.CAMERA
+
+    };
+    private static final int REQUEST_APP_PERMISSIONS = 0;
+    private final RequestPermissionDialog.OnClickListenerHandlerPermission m_onClickListenerHandlerPerm = new RequestPermissionDialog.OnClickListenerHandlerPermission() {
+
+        @Override
+        public void onClickListenerOk() {
+            ActivityCompat
+                    .requestPermissions(MainActivity.this, PERMISSIONS,
+                            REQUEST_APP_PERMISSIONS);
+        }
+    };
     //ad unit ca-app-pub-6508526601344465/6309563235
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,25 +100,42 @@ public class MainActivity extends AppCompatActivity {
         flashLightButton = (ImageButton)findViewById(R.id.flashlight_button);
         flashLightButton.setOnClickListener(new FlashOnOffListener());
         checkBox=(CheckBox)findViewById(R.id.checkBox);
-        if(Util.getInfo(getApplicationContext(),"background"))
-        {
-            checkBox.setChecked(true);
-        }
+
         checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 Log.e("MainActivity","Checked: "+b);
-                Util.saveInfo(getApplicationContext(),"background",b);
+                Util.saveInfo(MainActivity.this,"background",b);
                 if(b)
                 {
 //                    serviceIntent.putExtra("background",b);
 //                    startService(serviceIntent);
-//                    bindService(serviceIntent, flashServiceConnection, BIND_IMPORTANT);
+ //                  bindService(serviceIntent, flashServiceConnection, BIND_IMPORTANT);
                 }
             }
         });
         trun_on_off=(TextView)findViewById(R.id.turn_on_off);
 
+        if(checkPermission(Manifest.permission.CAMERA)) {
+            if (Util.getInfo(this, "background")) {
+                checkBox.setChecked(true);
+                serviceIntent = new Intent(MainActivity.this, FlashLightService.class);
+                startService(serviceIntent);
+                trun_on_off.setText(getResources().getString(R.string.turn_off));
+                flashLightButton.setImageResource(R.drawable.flashlight_off);
+                isFlashLightOn = false;
+                // bindService(serviceIntent, flashServiceConnection, BIND_IMPORTANT);
+            } else {
+                serviceIntent = new Intent(MainActivity.this, FlashLightService.class);
+                startService(serviceIntent);
+                trun_on_off.setText(getResources().getString(R.string.turn_off));
+                flashLightButton.setImageResource(R.drawable.flashlight_off);
+                isFlashLightOn = false;
+            }
+        }else
+        {
+            requestlocationPermissions();
+        }
 
         fm=(FrameLayout)findViewById(R.id.mainFrame);
         adView = new AdView(this);
@@ -122,15 +157,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        if(!Util.isServiceRunning(getApplicationContext(),FlashLightService.class.getName())) {
-            serviceIntent = new Intent(MainActivity.this, FlashLightService.class);
-            startService(serviceIntent);
-            trun_on_off.setText(getResources().getString(R.string.turn_off));
-    }
-        else
-        {
-            trun_on_off.setText(getResources().getString(R.string.turn_on));
-        }
+
     }
 
     private class FlashOnOffListener implements View.OnClickListener{
@@ -155,6 +182,7 @@ public class MainActivity extends AppCompatActivity {
                 trun_on_off.setText(getResources().getString(R.string.turn_on));
             }
             } catch (Exception e) {
+                Log.e("MainActivity","onClick: "+e);
                 e.printStackTrace();
             }
 
@@ -167,13 +195,92 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if(!Util.getInfo(getApplicationContext(),"background"))
+        try {
+            if (!Util.getInfo(this, "background")) {
+                stopService(serviceIntent);
+            }
+            //unbindService(flashServiceConnection);
+        }catch (Exception e)
         {
-            stopService(serviceIntent);
+
         }
         //close();
     }
+    private boolean checkPermission(String perm)
+    {
+        return(PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(this, perm));
+    }
 
+    private void requestlocationPermissions() {
+
+        // BEGIN_INCLUDE(contacts_permission_request)
+        Log.e(this.getClass().getSimpleName(), "Requesting location permission");
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.CAMERA)) {
+
+            // Provide an additional rationale to the user if the permission was not granted
+            // and the user would benefit from additional context for the use of the permission.
+            // For example, if the request has been denied previously.
+            Log.e(getClass().getSimpleName(),
+                    "Displaying contacts permission rationale to provide additional context.");
+            RequestPermissionDialog permissionDialog = new RequestPermissionDialog(
+                    MainActivity.this, m_onClickListenerHandlerPerm);
+
+            permissionDialog.setTitle(getResources().getString(R.string.grant));
+            permissionDialog.setMessage(getResources().getString(R.string.details));
+            WindowManager.LayoutParams wmlp = permissionDialog.getWindow()
+                    .getAttributes();
+            wmlp.gravity = Gravity.CENTER;
+            permissionDialog.show();
+
+        } else {
+            // Contact permissions have not been granted yet. Request them directly.
+            ActivityCompat.requestPermissions(this, PERMISSIONS, REQUEST_APP_PERMISSIONS);
+        }
+        // END_INCLUDE(contacts_permission_request)
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_APP_PERMISSIONS) {
+            boolean permissionGranted = true;
+            int index = 0;
+            for (int result : grantResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    permissionGranted = false;
+                    Log.e(this.getClass().getSimpleName(), "Permission failed :" + permissions[index]);
+                    break;
+                }
+                index++;
+            }
+            if (permissionGranted) {
+
+             if (Util.getInfo(this, "background")) {
+                    checkBox.setChecked(true);
+                    serviceIntent = new Intent(MainActivity.this, FlashLightService.class);
+                    startService(serviceIntent);
+                    trun_on_off.setText(getResources().getString(R.string.turn_off));
+                    flashLightButton.setImageResource(R.drawable.flashlight_off);
+                    isFlashLightOn = false;
+                    // bindService(serviceIntent, flashServiceConnection, BIND_IMPORTANT);
+                } else {
+                    serviceIntent = new Intent(MainActivity.this, FlashLightService.class);
+                    startService(serviceIntent);
+                    trun_on_off.setText(getResources().getString(R.string.turn_off));
+                    flashLightButton.setImageResource(R.drawable.flashlight_off);
+                    isFlashLightOn = false;
+                }
+
+            }
+            else {
+
+                finish();
+            }
+        }
+        else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
 
 
 
